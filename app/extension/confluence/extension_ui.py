1f60ed648890bbb5dbafdd137e0db7c39fc655ce
Darryl Duke <darryl@brikit.com>
2021-02-15 13:06:26 -0800

    Add app-specific tests for Content Flow

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -19,6 +19,8 @@ def app_specific_action(webdriver, datasets):
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, ".calendar-integrations-macro"))  # Wait for the Calendar Integrations macros
+            page.wait_until_visible((By.ID, "content-flow-page-status-banner"))  # Wait for the root Content Flow metadata banner element
+            page.wait_until_clickable((By.ID, "action-menu-link")).click()
+            page.wait_until_visible((By.ID, "content-flow-schedule-page"))  # Wait for the Schedule Page menu item
         sub_measure()
     measure()
